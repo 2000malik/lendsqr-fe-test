432e053d5f0e01d9a0d4c21e3b0f1cbf233e165e
Malik <36223770+2000malik@users.noreply.github.com>
2025-07-06 11:42:58 +0100

    patch: side bar layout container design

--- src/components/layouts/sidebar/index.tsx
+++ src/components/layouts/sidebar/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+// 
+import styles from "./styles.module.scss"
 export const SideBar: React.FC = () => {
-  return <aside>index</aside>;
+  return <aside className={styles.sidebarContainer}>index
+  
+  knkmnkkmk Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, ratione inventore voluptas unde et nesciunt deserunt, itaque minus atque velit optio nam laboriosam culpa eveniet quasi adipisci beatae impedit esse.
+  </aside>;
 };
