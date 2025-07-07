2153f00fe911c4cac08f1286090bad6cbd47263d
Malik <36223770+2000malik@users.noreply.github.com>
2025-07-07 09:50:46 +0100

    feat: users page layout structure

--- src/features/users/index.tsx
+++ src/features/users/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-// 
-import { Header } from '@/src/components'
+"use client";
+import React from "react";
+//
+import { Button, Header, SummaryCard } from "@/src/components";
+//
+import styles from "./styles.module.scss";
+import { DashboardTable } from "../dashboard/table";
+import { summaryCardData, tableData } from "../dashboard/data";
 
 export const Users = () => {
   return (
-    <div>
-      <Header title='Users'/>
+    <div className={styles.dashboardContainer}>
+      <Header title="Users" />
+      <div className={styles.cardGrid}>
+        {summaryCardData.map(({ label, Icon, value }) => (
+          <SummaryCard key={label} label={label} Icon={Icon} value={value} />
+        ))}
+      </div>
+      <div className={styles.filterButtonContainer}>
+        <Button className={styles.filterBtn}>Filter</Button>
+      </div>
+        <DashboardTable data={tableData} />
     </div>
-  )
-}
+  );
+};
