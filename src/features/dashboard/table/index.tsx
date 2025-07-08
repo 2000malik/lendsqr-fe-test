648c7cdf4ee1a66c94fa65be1ff8e37cd2404250
Malik <36223770+2000malik@users.noreply.github.com>
2025-07-09 00:53:52 +0100

    patch: custom hook for setting & getting value in local storage

--- src/features/dashboard/table/index.tsx
+++ src/features/dashboard/table/index.tsx
@@ -7,10 +7,14 @@ import { StatusChip, Table } from "@/src/components";
 import { ColumnConfig } from "@/src/components/table/column";
 //
 import styles from "../styles.module.scss";
+import { setStorage } from "@/src/libs/utils";
+import { useRouter } from "next/navigation";
+import { RoutePath } from "@/src/constants/routes";
 
 type Props = { data: userInfo[]; loading?: boolean };
 
 export const DashboardTable: React.FC<Props> = ({ data, loading }) => {
+  const router = useRouter();
   const tableHeaders: ColumnConfig[] = [
     { key: "organization", label: "organization" },
     { key: "username", label: "Username" },
@@ -25,10 +29,16 @@ export const DashboardTable: React.FC<Props> = ({ data, loading }) => {
     {
       key: "actions",
       label: "",
-      render: (_: unknown, row: { username: string }) => (
+      render: (_: unknown, row: userInfo) => (
         <button
           className={styles.viewMoreBtn}
-          onClick={() => alert(row.username)}
+          onClick={() => {
+            setStorage({
+              key: "user",
+              data: row,
+              next: () => router.push(`${RoutePath.USER}/${row.id}`),
+            });
+          }}
         >
           <MoreIcon />
         </button>
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,9 @@
+type StorageType = {
+  key: string;
+  data: Object;
+  next?: () => void;
+};
+
 export const isActiveLink = ({
   link,
   pathname,
@@ -9,4 +15,24 @@ export const isActiveLink = ({
     return pathname === link;
   }
   return pathname?.startsWith(`${link}`);
+};
+
+export const setStorage = ({ key, data, next }: StorageType) => {
+  if (typeof window !== "undefined") {
+    let user = JSON.stringify(data);
+    // can further encrypt data
+    localStorage.setItem(key, user);
+  }
+  next?.();
+};
+
+export const getStorageItem = (key: string) => {
+  try {
+    if (typeof window !== "undefined") {
+      const data = localStorage.getItem(key) || "";
+      return data && JSON.parse(data);
+    }
+  } catch (error) {
+    localStorage.removeItem(key);
+  }
 };
